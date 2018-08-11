22ef7fa9c69a6b454f96f5e128ed98bc16a8b44f
baldwin <niceboybao@gmail.com>
2018-08-11 18:08:38 +0800

    webpack init@1.0

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,15 @@
  * 妈妈再也不用担心我不会webpack了2 https://juejin.im/post/5a17a9ef51882540f3633bff
 */
 
-module.exports = {
+//整个webpack配置对象
+var webpackConfig = {
+	//入口
 	entry: './src/index.js', // 这里是项目入口文件地址
+	//出口
 	ouput: {
-		path: __dirname + '/dist', // 这里是项目输出的路径,__dirname表示当前文件的位置
+		path: __dirname + '/www', // 这里是项目输出的路径,__dirname表示当前文件的位置
 		filename: 'js/' + '[name].js' // 这里是生成文件的名称，可起你想要的名字
 	}
 };
+
+module.exports = webpackConfig;
