eff931901a610323cdb92d3180a999ad2591229e
baldwin <niceboybao@gmail.com>
2018-09-03 18:32:15 +0800

    ［需求／BUG］: webpack 处理css sass 图片等loader ［模块］: 无 ［说明］: 无

---
+++ docs/webpack.md
@@ -0,0 +1,4 @@
+# webpack
+
+[深入浅出 Webpack小册](http://webpack.wuhaolin.cn/)
+
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@
  * @Author: guangwei.bao 
  * @Date: 2018-08-30 20:11:11 
  * @Last Modified by: guangwei.bao
- * @Last Modified time: 2018-08-31 14:36:05
+ * @Last Modified time: 2018-09-03 17:45:26
  * @Describe: 工程入口文件
  */
 
-
 // 通过 CommonJS 规范导入 CSS 模块
 // require('./index.css');
-import './index.css';
+import './styles/index.css';
+import './styles/index.scss';
 
-
-document.write('<div>12341ß1</div>');
+let app = '123';
+let func = () => {
+	return 'hello baldwin';
+};
