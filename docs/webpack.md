749dae3712f1302c445208be32aaa62091b310b2
baldwin <niceboybao@gmail.com>
2018-09-06 20:43:52 +0800

    ［需求／BUG］: webpack 测试脚本 ［模块］: 无 ［说明］: 无

--- docs/webpack.md
+++ docs/webpack.md
@@ -2,3 +2,5 @@
 
 [深入浅出 Webpack小册](http://webpack.wuhaolin.cn/)
 [升级到Webpack 4.x初步踩坑总结](https://blog.csdn.net/harsima/article/details/80819747)
+[Webpack 优秀周边生态收集。](https://github.com/webpack-contrib/awesome-webpack)
+[Webpack 优秀中文文章。](https://github.com/webpack-china/awesome-webpack-cn)
--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,14 @@
  * @Author: guangwei.bao 
  * @Date: 2018-09-05 20:10:44 
  * @Last Modified by: guangwei.bao
- * @Last Modified time: 2018-09-05 20:19:55
+ * @Last Modified time: 2018-09-06 20:11:58
  * @Describe: 工程入口文件
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/index.scss';
+import './styles/index.css';
 
 ReactDOM.render(
 	<div>hello world</div>,
