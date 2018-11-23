6a68bf39f531f8d1aa894e8925c770f78e6b1902
baldwin <niceboybao@gmail.com>
2018-11-23 15:58:07 +0800

    ［需求／BUG］: 无 ［模块］: 无 ［说明］: 无

--- src/containers/Demos/Iframe/index.jsx
+++ src/containers/Demos/Iframe/index.jsx
@@ -2,7 +2,7 @@
  * @Author: guangwei.bao 
  * @Date: 2018-10-31 17:40:12 
  * @Last Modified by: guangwei.bao
- * @Last Modified time: 2018-11-21 17:28:40
+ * @Last Modified time: 2018-11-23 15:58:00
  * @Describe: iframe和postmessage配合实现2个嵌套页面通信
  */
 
@@ -12,7 +12,7 @@ import { connect } from 'react-redux';
 import config from '../../../config.js';
 import utils from '../../../utils';
 
-const URL_GIS_BASE = document.location.protocol == 'http:' ? config.GIS_LINK : config.GIS_LINK_HTTPS;
+const URL_GIS_BASE = document.location.protocol == 'http:' ? config.GIS_LINK : config.GIS_LINK;
 export default class Iframe extends React.Component {
 	constructor(props) {
 		super(props);
