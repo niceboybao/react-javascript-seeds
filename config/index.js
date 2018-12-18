8fe090191a8aac52ba0c74ee7096d04534322f45
baldwin <niceboybao@gmail.com>
2018-12-18 16:40:14 +0800

    ［需求／BUG］: PACKAGE_PATH 配置 ［模块］: 无 ［说明］: 无

--- config/index.js
+++ config/index.js
@@ -2,18 +2,22 @@
  * @Author: guangwei.bao 
  * @Date: 2018-08-30 20:09:25 
  * @Last Modified by: guangwei.bao
- * @Last Modified time: 2018-12-17 21:02:25
+ * @Last Modified time: 2018-12-18 16:39:25
  * @Describe: webpack 配置文件
  */
 'use strict';
 
 // 获取开发环境变量
-// console.log('工程启动环境: ' + process.env.NODE_ENV);
+console.log('工程启动环境: ' + process.env.NODE_ENV);
 const NODE_ENV = process.env.NODE_ENV;
+
 // 工程打包到的文件夹名称
 // 部署到服务器上的时候，可以根据需求改变打包文件夹名称，一改全改
-// let PACKAGE_PATH = 'www'; //开发环境 生产环境 线上环境
-let PACKAGE_PATH = 'react-javascript-seeds'; //开发环境 生产环境 线上环境
+let PACKAGE_PATH = 'www'; //开发环境 生产环境
+if (NODE_ENV === 'production_online') {
+	PACKAGE_PATH = 'react-javascript-seeds'; // 线上环境
+}
+console.log('工程打包部署路径: ' + PACKAGE_PATH);
 
 // 根据开发环境设置前缀
 const getWebpackpublicPath = () => {
